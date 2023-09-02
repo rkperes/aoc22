f4e7c16539c9c697fcf6c5667a22f8f17a78ea61
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-09-02 20:15:01 -0300

    Solve day 1, problem 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,6 +2,47 @@ use std::io;
 use std::io::BufRead;
 
 fn main() -> io::Result<()> {
+    // problem1().unwrap();
+    problem2().unwrap();
+    Ok(())
+}
+
+fn problem2() -> io::Result<()> {
+    let reader = io::BufReader::new(io::stdin());
+
+    let top3: &mut [i32] = &mut [0; 3];
+
+    let mut acc: i32 = 0;
+
+    for line in reader.lines() {
+        match line?.trim().parse::<i32>() {
+            Ok(val) => acc += val,
+            Err(_) => {
+                update_top_n(top3, acc);
+                acc = 0;
+            }
+        };
+    }
+    update_top_n(top3, acc);
+
+    println!("Problem 2: {}", top3[0] + top3[1] + top3[2]);
+    Ok(())
+}
+
+fn update_top_n(top_n: &mut [i32], val: i32) {
+    let mut next = val;
+
+    for i in 0..top_n.len() {
+        let cur = *top_n.get(i).unwrap();
+        if next > cur {
+            let tmp: i32;
+            (tmp, next) = (next, cur);
+            top_n[i] = tmp;
+        }
+    }
+}
+
+fn problem1() -> io::Result<()> {
     let reader = io::BufReader::new(io::stdin());
 
     let mut acc: i32 = 0;
