0d23fc694d979a184374f7227cb0a27391af6f15
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-09-07 09:20:40 -0300

    Solve day 2, problem 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -2,9 +2,33 @@ use std::io;
 use std::io::BufRead;
 
 fn main() -> io::Result<()> {
-    problem1().unwrap();
-    // problem2().unwrap();
+    // problem1().unwrap();
+    problem2().unwrap();
     Ok(())
+}
+
+fn problem2() -> io::Result<()> {
+    let reader = io::BufReader::new(io::stdin());
+
+    let mut acc: u32 = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        acc += points_from_match2(line);
+    }
+
+    println!("Problem 2: {}", acc);
+
+    Ok(())
+}
+
+fn points_from_match2(input: String) -> u32 {
+    let mut plays = input.split_whitespace();
+
+    let theirs = play_from(plays.next().unwrap());
+    let mine = decide_play(&theirs, plays.next().unwrap());
+
+    mine as u32 + mine.score_vs(theirs)
 }
 
 fn problem1() -> io::Result<()> {
@@ -51,6 +75,31 @@ impl Play {
 
             _ => 0,
         }
+    }
+
+    fn loses(&self) -> Play {
+        match self {
+            Play::Rock => Play::Paper,
+            Play::Paper => Play::Scissors,
+            Play::Scissors => Play::Rock,
+        }
+    }
+
+    fn wins(&self) -> Play {
+        match self {
+            Play::Rock => Play::Scissors,
+            Play::Paper => Play::Rock,
+            Play::Scissors => Play::Paper,
+        }
+    }
+}
+
+fn decide_play(base: &Play, decision: &str) -> Play {
+    match decision {
+        "X" => base.wins(),
+        "Y" => *base,
+        "Z" => base.loses(),
+        _ => panic!("bad play {}", decision),
     }
 }
 
