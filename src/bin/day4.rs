7be7955a4773dfbd98e5fa36730c6d1d36b466c1
Rafael Koch Peres <rafaelkperes@gmail.com>
2023-10-21 13:29:24 -0300

    Solve day 4, problem 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,8 +1,9 @@
 use aoc22::ioutil;
-use std::io;
+use core::fmt;
+use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
-    problem1();
+    problem2();
 
     Ok(())
 }
@@ -22,9 +23,33 @@ fn problem1() {
     println!("Problem 1: {}", acc)
 }
 
+fn problem2() {
+    let lines = ioutil::stdin_as_lines();
+
+    let mut acc: i32 = 0;
+
+    for line in lines {
+        let p = Pair::from_string(line);
+        if p.overlaps_self() {
+            // println!("overlaps {}", p);
+            acc += 1;
+        } else {
+            // println!("not overlaps {}", p);
+        }
+    }
+
+    println!("Problem 2: {}", acc)
+}
+
 struct Pair {
     first: Range,
     second: Range,
+}
+
+impl fmt::Display for Pair {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.first, self.second)
+    }
 }
 
 impl Pair {
@@ -43,9 +68,19 @@ impl Pair {
     pub fn contains_self(&self) -> bool {
         self.first.contains(&self.second) || self.second.contains(&self.first)
     }
+
+    pub fn overlaps_self(&self) -> bool {
+        self.first.overlaps(&self.second)
+    }
 }
 
 struct Range(i32, i32);
+
+impl fmt::Display for Range {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}-{}", self.0, self.1)
+    }
+}
 
 impl Range {
     pub fn new(start: i32, end: i32) -> Self {
@@ -62,5 +97,9 @@ impl Range {
 
     pub fn contains(&self, other: &Range) -> bool {
         self.0 <= other.0 && self.1 >= other.1
+    }
+
+    pub fn overlaps(&self, other: &Range) -> bool {
+        (self.1 >= other.0 && self.1 <= other.1) || (other.1 >= self.0 && other.1 <= self.1)
     }
 }
